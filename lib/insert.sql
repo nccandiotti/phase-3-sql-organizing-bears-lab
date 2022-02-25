4e896bbb31b8aaad5012b48dc523d470e030cc50
Nicole Candiotti <nccandiotti@gmail.com>
2022-02-25 11:45:45 -0500

    commit tree b0e6af28ab6e19c09bd0218afc19443200262ea7

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears VALUES (1, "MR. CHOCOLATE", 1, "M", "BROWN", "CALM", 0);
+INSERT INTO bears VALUES (2, "rowdy", 2, "M", "BROWN", "CALM", 0);
+INSERT INTO bears VALUES (3, "Tabitha", 2, "M", "BROWN", "CALM", 1);
+INSERT INTO bears VALUES (4, "Sergeant Brown", 2, "M", "BROWN", "CALM", 1);
+INSERT INTO bears VALUES (5, "Melissa", 2, "M", "BROWN", "CALM", 0);
+INSERT INTO bears VALUES (6, "grinch", 2, "M", "BROWN", "CALM",1);
+INSERT INTO bears VALUES (7, "Wendy", 2, "M", "BROWN", "CALM",1);
+INSERT INTO bears  VALUES(8, null, 2, "M", "BROWN", "CALM", 1);
+
+
